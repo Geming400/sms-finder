0871d9dcc4469c48f85940e15e846924c4e39f8e
Geming400 <Geming400@gmail.com>
2026-06-13 19:33:03 +0200

    added 'SMS' log tag

--- app/src/main/java/fr/geming400/localisationhelper/LogTags.java
+++ app/src/main/java/fr/geming400/localisationhelper/LogTags.java
@@ -3,6 +3,7 @@ package fr.geming400.localisationhelper;
 public final class LogTags {
     public static final String SMS_PERMISSIONS = "smsPermissions";
     public static final String SMS_RECEIVER = "smsReceiver";
+    public static final String SMS_SENDER = "SMS";
     public static final String USER_TRACKING = "userTracking";
     public static final String OSM_ACTIVITY = "OSMActivity";
 }
