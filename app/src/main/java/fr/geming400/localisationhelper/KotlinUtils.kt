efdd8be67a432fe7f57a68a3f806d2ed4c6e31b4
Geming400 <Geming400@gmail.com>
2026-06-13 19:33:11 +0200

    Added some utils

---
+++ app/src/main/java/fr/geming400/localisationhelper/KotlinUtils.kt
@@ -0,0 +1,13 @@
+package fr.geming400.localisationhelper
+
+import androidx.compose.foundation.text.input.TextFieldState
+
+/**
+ * Sets the text of this [TextFieldState] to `text`
+ * @param text the text to set
+ */
+fun TextFieldState.set(text: String) {
+    edit {
+        replace(0, length, text)
+    }
+}
--- app/src/main/java/fr/geming400/localisationhelper/Utils.java
+++ app/src/main/java/fr/geming400/localisationhelper/Utils.java
@@ -3,8 +3,24 @@ package fr.geming400.localisationhelper;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.telephony.PhoneNumberUtils;
+import android.telephony.SmsManager;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
+
+import org.jetbrains.annotations.Unmodifiable;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
+import contacts.core.entities.Contact;
+import contacts.core.entities.Phone;
+import contacts.core.entities.RawContact;
 
 public final class Utils {
     private Utils() {}
@@ -38,5 +54,109 @@ public final class Utils {
         } catch (NumberFormatException e) {
             return false;
         }
+    }
+
+
+    public static String formatNumber(@Nullable String number, @NonNull String defaultNum) {
+        if (number == null)
+             return null;
+
+        String res = PhoneNumberUtils.formatNumber(number, Locale.getDefault().getISO3Language());
+        return res == null ? defaultNum : res;
+    }
+    @Nullable
+    public static String formatNumber(@Nullable String number) {
+        return formatNumber(number, "INVALID NUMBER");
+    }
+
+    @NonNull
+    public static String formatNumber(Phone phone) {
+        String res = PhoneNumberUtils.formatNumber(
+                phone.getNormalizedNumber() == null ? "" : phone.getNormalizedNumber(),
+                Locale.getDefault().getISO3Language()
+        );
+
+        return res == null
+                ? (phone.getNumber() == null ? "Unknown number" : phone.getNumber())
+                : res;
+    }
+
+    public static boolean isNumberValid(@Nullable String number) {
+        if (number == null)
+            return false;
+
+        return PhoneNumberUtils.formatNumber(number, Locale.getDefault().getISO3Language()) == null;
+    }
+    public static boolean isNumberValid(@Nullable Phone phone) {
+        if (phone == null)
+            return false;
+
+        return PhoneNumberUtils.formatNumber(phone.getNormalizedNumber(), Locale.getDefault().getISO3Language()) != null;
+    }
+
+    public static boolean hasAnyValidNumber(@Nullable Contact contact) {
+        if (contact == null)
+            return false;
+
+        for (RawContact rawContact : contact.getRawContacts()) {
+            for (Phone phone : rawContact.getPhones()) {
+                if (isNumberValid(phone))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean areAllNumbersValid(@Nullable Contact contact) {
+        if (contact == null)
+            return false;
+
+        for (RawContact rawContact : contact.getRawContacts()) {
+            for (Phone phone : rawContact.getPhones()) {
+                if (!isNumberValid(phone))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Nullable
+    public static Phone getFirstValidPhone(@Nullable Contact contact) {
+        if (contact == null)
+            return null;
+
+        for (RawContact rawContact : contact.getRawContacts()) {
+            for (Phone phone : rawContact.getPhones()) {
+                if (isNumberValid(phone))
+                    return phone;
+            }
+        }
+
+        return null;
+    }
+
+    @Unmodifiable
+    public static Collection<Phone> getPhones(@Nullable Contact contact, boolean mustBeValid) {
+        if (contact == null)
+            return Set.of();
+
+        Set<Phone> phones = new HashSet<>();
+        for (RawContact rawContact : contact.getRawContacts()) {
+            for (Phone phone : rawContact.getPhones()) {
+                if (mustBeValid && isNumberValid(phone))
+                    phones.add(phone);
+            }
+        }
+
+        return phones;
+    }
+
+    public static void sendSMS(@NonNull Context context, @NonNull String sender, String body) {
+        Log.i(LogTags.SMS_SENDER, "Sending sms to" + sender + "with body: " + body);
+
+        var smsManager = context.getSystemService(SmsManager.class);
+        smsManager.sendMultipartTextMessage(sender, null, smsManager.divideMessage(body), null, null);
     }
 }
