4a00cfacc6b97f6efe22541fddded0a941c504fe
Geming400 <Geming400@gmail.com>
2026-06-13 19:32:32 +0200

    Finished tracking activity

--- app/src/main/java/fr/geming400/localisationhelper/ui/activities/TrackingActivity.kt
+++ app/src/main/java/fr/geming400/localisationhelper/ui/activities/TrackingActivity.kt
@@ -1,6 +1,8 @@
 package fr.geming400.localisationhelper.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.setContent
@@ -8,27 +10,66 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.launch
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
+import contacts.core.Contacts
+import contacts.core.LookupQuery
+import fr.geming400.localisationhelper.R
+import fr.geming400.localisationhelper.Utils
+import fr.geming400.localisationhelper.datastore.JsonDataStore
 import fr.geming400.localisationhelper.ui.components.ActivitySelector
 import fr.geming400.localisationhelper.ui.components.AppDestinations
 import fr.geming400.localisationhelper.ui.components.ContactsList
 import fr.geming400.localisationhelper.ui.theme.LocalisationHelperTheme
+import kotlinx.coroutines.runBlocking
 
 class TrackingActivity : ComponentActivity() {
     val contactPickerLauncher: ActivityResultLauncher<Void?> =
         registerForActivityResult(
             ActivityResultContracts.PickContact()
         ) { result ->
+            var jsonDataStore = JsonDataStore(this)
 
+            // when result == null, the user chose no contacts
+            if (result != null) {
+                val contactId = result.toString().substringAfterLast("/").toLong()
+                val lookupKey = result.toString().substringAfter("lookup/").substringBefore("/")
+                var lookupKeyWithId = LookupQuery.LookupKeyWithId(lookupKey, contactId)
+
+                val contact = Contacts(this)
+                    .lookupQuery()
+                    .whereLookupKeyWithIdMatches(lookupKeyWithId)
+                    .find()
+                    .first()
+
+                if (Utils.hasAnyValidNumber(contact)) {
+                    runBlocking {
+                        jsonDataStore.addTrackedContact(lookupKeyWithId)
+                        jsonDataStore.updateTrackedContact(contact) {
+                            it.copy(linkedPhoneNumber = Utils.getFirstValidPhone(contact)?.normalizedNumber)
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, R.string.has_no_valid_numbers_error, Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
         }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,31 +77,52 @@ class TrackingActivity : ComponentActivity() {
 
         enableEdgeToEdge()
         setContent {
+            val coroutineScope = rememberCoroutineScope()
+            val jsonDataStore = remember(this) { JsonDataStore(this) }
+
+            val trackedContacts by jsonDataStore.trackedContactsFlow()
+                .collectAsState(initial = emptyList(), coroutineScope.coroutineContext)
+
             LocalisationHelperTheme {
                 ActivitySelector(AppDestinations.TRACKING) {
                     Scaffold(
                         modifier = Modifier.fillMaxSize(),
-                        bottomBar = { BottomBar() }
+                        floatingActionButton = { ContactAdderButton() }
                     ) { innerPadding ->
-                        ContactsList(modifier = Modifier.padding(innerPadding))
+                        ContactsList(
+                            modifier = Modifier.padding(innerPadding),
+                            contactsFilter = trackedContacts.map { it.lookupKeyWithId },
+                            onContactClick = { contact ->
+                                val intent = Intent(this, UserTrackingActivity::class.java)
+                                val bundle = Bundle()
+                                bundle.putString("lookupKey", contact.lookupKey)
+                                bundle.putLong("contactID", contact.id)
+                                intent.putExtras(bundle)
+
+                                this.startActivity(intent)
+                            }
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .padding(innerPadding)
+                                    .padding(horizontal = 8.dp)
+                                    .fillMaxSize(),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Column() {
+                                    Text(
+                                        stringResource(R.string.add_contacts),
+                                        fontSize = 4.em,
+                                        textAlign = TextAlign.Center
+                                    )
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
     }
-}
-
-@Composable
-fun BottomBar() {
-    BottomAppBar(
-        actions = {
-
-        },
-        floatingActionButton = {
-            ContactAdderButton()
-        },
-        containerColor = Color.Transparent
-    )
 }
 
 @Composable
