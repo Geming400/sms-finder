c74e66805960f0bc21db26a7b34edbd4931f3b80
Geming400 <Geming400@gmail.com>
2026-06-15 15:57:06 +0200

    Storing last ping timestamp

--- app/src/main/java/fr/geming400/localisationhelper/datastore/TrackingData.kt
+++ app/src/main/java/fr/geming400/localisationhelper/datastore/TrackingData.kt
@@ -1,10 +1,10 @@
 package fr.geming400.localisationhelper.datastore
 
 import android.content.Context
-import androidx.annotation.IntRange
 import contacts.core.Contacts
 import contacts.core.LookupQuery
 import contacts.core.entities.Contact
+import fr.geming400.localisationhelper.utils.Timestamp
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -14,7 +14,8 @@ data class TrackingData(
     val linkedPhoneNumber: String? = null,
 
     // Tracking info
-    val geolocation: SerializableGeolocation? = null
+    val geolocation: SerializableGeolocation? = null,
+    val lastPingAnswer: Timestamp? = null
 ) {
     val lookupKeyWithId: LookupQuery.LookupKeyWithId
         get() = LookupQuery.LookupKeyWithId(lookupKey, 0)
@@ -34,10 +35,7 @@ data class SerializableGeolocation(
     val longitude: Double,
     val latitude: Double,
     /**
-     * When this geolocation data got recorded. If left to `0` then the date is unknown.
-     *
-     * Value is a timestamp
+     * When this geolocation data got recorded
      */
-    @field:IntRange(from = 0)
-    val lastTimeRecorded: Long = 0
+    val lastTimeRecorded: Timestamp? = null
 )
