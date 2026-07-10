0e9d9d4459919478c2153353231c5e04554ea3f0
Geming400 <Geming400@gmail.com>
2026-07-11 00:02:54 +0200

    Added some utils

--- app/src/main/java/fr/geming400/localisationhelper/utils/BoxedTimestamp.kt
+++ app/src/main/java/fr/geming400/localisationhelper/utils/BoxedTimestamp.kt
@@ -1,11 +1,17 @@
 package fr.geming400.localisationhelper.utils
 
+import androidx.annotation.NonNull
 import kotlinx.serialization.Serializable
+import java.util.Objects
 
 @Serializable
 data class BoxedTimestamp<out T>(val timestamp: Timestamp, val value: T) {
     fun getRelativeTime() =
         this.timestamp.getRelativeTime()
+
+    @NonNull
+    fun getOrThrow(): T
+        = Objects.requireNonNull(this.value, "Tried unboxing value of boxed timestamp $this but failed because value was null !")
 
     companion object {
         fun <T> now(value: T): BoxedTimestamp<T> {
--- app/src/main/java/fr/geming400/localisationhelper/utils/KotlinUtils.kt
+++ app/src/main/java/fr/geming400/localisationhelper/utils/KotlinUtils.kt
@@ -1,12 +1,19 @@
 package fr.geming400.localisationhelper.utils
 
+import androidx.annotation.IntRange
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalResources
+import androidx.compose.ui.res.stringResource
+import fr.geming400.localisationhelper.R
 
 /**
  * Sets the text of this [TextFieldState] to `text`
@@ -26,3 +33,34 @@ fun Modifier.centerVertically(fraction: Float = 1f, unbounded: Boolean = false):
     this
         .fillMaxHeight(fraction)
         .wrapContentHeight(Alignment.CenterVertically, unbounded)
+
+@IntRange(from = 0, to = 1)
+fun Boolean.toInt(): Int =
+    if (this) 1 else 0
+
+fun Int.toBool(): Boolean =
+    this == 1
+
+fun String.toBoolFromInt(): Boolean =
+    this.toInt().toBool()
+
+@Composable
+@ReadOnlyComposable
+fun nullableStringResource(@StringRes id: Int, vararg formatArgs: Any?, default: Any = "null"): String {
+    val nullsafeFormatArgs = formatArgs.map {
+        it ?: default
+    }
+
+    return LocalResources.current.getString(id, *nullsafeFormatArgs.toTypedArray())
+}
+
+@Composable
+@ReadOnlyComposable
+fun getYesOrNo(bool: Boolean?): String {
+    return if (bool == true)
+        stringResource(R.string.yes)
+    else if (bool == false)
+        stringResource(R.string.no)
+    else
+        stringResource(R.string.unknown)
+}
