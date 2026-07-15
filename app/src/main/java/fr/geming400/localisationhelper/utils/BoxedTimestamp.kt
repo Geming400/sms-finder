1dcf9688deeddbe2d11b9b9f4a8f15082dee6a4c
Geming400 <geming400@gmail.com>
2026-07-15 22:15:21 +0200

    Added util to make null BoxedTimestamp

--- app/src/main/java/fr/geming400/localisationhelper/utils/BoxedTimestamp.kt
+++ app/src/main/java/fr/geming400/localisationhelper/utils/BoxedTimestamp.kt
@@ -17,5 +17,9 @@ data class BoxedTimestamp<out T>(val timestamp: Timestamp, val value: T) {
         fun <T> now(value: T): BoxedTimestamp<T> {
             return BoxedTimestamp(Timestamp.now(), value)
         }
+
+        fun <T> nowNull(): BoxedTimestamp<T?> {
+            return BoxedTimestamp(Timestamp.now(), null)
+        }
     }
 }
