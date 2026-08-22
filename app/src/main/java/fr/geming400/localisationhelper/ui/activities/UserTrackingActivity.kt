0bbdefb7fa4ae4f3d2ba24eec2233bcce03736fb
Geming400 <Geming400@gmail.com>
2026-08-22 17:23:33 +0200

    Removed unused import

--- app/src/main/java/fr/geming400/localisationhelper/ui/activities/UserTrackingActivity.kt
+++ app/src/main/java/fr/geming400/localisationhelper/ui/activities/UserTrackingActivity.kt
@@ -7,7 +7,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.util.TypedValue
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
